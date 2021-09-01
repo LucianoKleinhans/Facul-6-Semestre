6302f1d0664ba91b03c0e7d5e0847d99093c7b9e
Luciano Kleinhans <72584236+LucianoKleinhans@users.noreply.github.com>
2021-08-31 20:01:37 -0400

    attex

--- Felipe-DevWeb/Atividades-Web/ListaAtv/ex15.php
+++ Felipe-DevWeb/Atividades-Web/ListaAtv/ex15.php
@@ -1,13 +1,13 @@
 <!-- Faça uma função que receba um valor inteiro e positivo e calcula e retorne o seu fatorial. Não utilize funções prontas de fatorial. -->
 <?php
-function Fatorial($Numero)
+function Fatorial($numero)
 {
-    if (is_int($Numero)) {
-        if ($Numero > 0) {
-            for ($Fatorial = 1; $Numero > 1; $Numero = $Numero - 1) {
-                $Fatorial = $Fatorial * $Numero;
+    if (is_int($numero)) {
+        if ($numero > 0) {
+            for ($fatorial = 1; $numero > 1; $numero = $numero - 1) {
+                $fatorial = $fatorial * $numero;
             }
-            echo " O fatorial do numero é de: " . $Fatorial;
+            echo " O fatorial do numero ' ".$numero." ' é de: ".$fatorial;
         }
     }
 }
